40b7c30b9cac53ea59bdcfa6f9de7a69b99237e1
Alexandre Buffet <43843473+alexandrebuffet@users.noreply.github.com>
2026-07-02 14:44:35 +0200

    chore(release): bump tested up to WordPress 7.0
    * chore: release 1.2.0 — bump tested up to WordPress 7.0
    * chore(changelog): remove unreleased section

--- embed-block-for-matterport.php
+++ embed-block-for-matterport.php
@@ -9,7 +9,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Embed Block for Matterport
- * Version:           1.1.1
+ * Version:           1.2.0
  * Plugin URI:        https://github.com/alexandrebuffet/embed-block-for-matterport
  * Description:       Adds Matterport oEmbed support and provides a new Embed block variation.
  * Author:            Alexandre Buffet
